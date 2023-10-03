b3925f159b4eb84145f4b43c19bb73e683bd94ec
gdozdjov <german.dozdjov@gmail.com>
2023-10-03 16:30:42 +0300

    modified:   cypress/e2e/subscribe.cy.ts 	modified:   cypress/support/commands.ts

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -2,25 +2,32 @@ describe('Subscription Page', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000')
     });
-    it('Test 1: Valid email for subscription', () => {
-        cy.getByData('email-input').type('tom@aol.com')
-        cy.getByData('submit-button').click()
-        cy.getByData('success-message').should('exist').contains('tom@aol.com')
+
+    context('E-mail Validation', () => {
+        it('Test 1: Valid email for subscription', () => {
+            cy.getByData('email-input').type('tom@aol.com')
+            cy.getByData('submit-button').click()
+            cy.getByData('success-message').should('exist').contains('tom@aol.com')
+        });
+
+        it('Test 2: Invalid e-mail for subscription', () => {
+            cy.getByData('email-input').type('tom')
+            cy.getByData('submit-button').click()
+            cy.getByData('success-message').should('not.exist')
+        });
+
+        it('Test 3: Duplicate e-mail subscription', () => {
+            cy.getByData('email-input').type('john@example.com')
+            cy.getByData('submit-button').click()
+            cy.getByData('success-message').should('not.exist')
+            cy.getByData('server-error-message')
+                .should('exist')
+                .contains('Error: john@example.com already exists. Please use a different email address.')
+        });
     });
 
-    it('Test 2: Invalid e-mail for subscription', () => {
-        cy.getByData('email-input').type('tom')
-        cy.getByData('submit-button').click()
-        cy.getByData('success-message').should('not.exist')
-    });
+    context('Page Validation', () => {
 
-    it('Test 3: Duplicate e-mail subscription', () => {
-        cy.getByData('email-input').type('john@example.com')
-        cy.getByData('submit-button').click()
-        cy.getByData('success-message').should('not.exist')
-        cy.getByData('server-error-message')
-            .should('exist')
-            .contains('Error: john@example.com already exists. Please use a different email address.')
     });
 
 });
